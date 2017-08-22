ae76c8f5b3e05da28d7a5f2d0c6caa1177c4ac30
Andrej Saibel <andrej.saibel@cern.ch>
2017-08-22 13:41:11 +0200

    working config for 7.1.1

--- GeneratorInterface/Herwig7Interface/test/Herwigpp_ConfigFile_cff_py_GEN_SIM.py
+++ GeneratorInterface/Herwig7Interface/test/Herwigpp_ConfigFile_cff_py_GEN_SIM.py
@@ -81,15 +81,15 @@ process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
 # Other statements
 process.genstepfilter.triggerConditions=cms.vstring("generation_step")
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'MCRUN2_71_V1::All', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
 
 process.generator = cms.EDFilter("Herwig7GeneratorFilter",
     run = cms.string('InterfaceMatchboxTest'),
-    repository = cms.string('HerwigDefaults.rpo'),
-    dataLocation = cms.string('${HERWIGPATH}'),
+    repository = cms.string('${HERWIGPATH}/HerwigDefaults.rpo'),
+    dataLocation = cms.string('${HERWIGPATH:-6}'),
     generatorModule = cms.string('/Herwig/Generators/EventGenerator'),
     eventHandlers = cms.string('/Herwig/EventHandlers'),
-    configFiles = cms.vstring('TestConfig.in',),
+    configFiles = cms.vstring('LHC-Matchbox.in',),
     crossSection = cms.untracked.double(-1),
     parameterSets = cms.vstring(),
     filterEfficiency = cms.untracked.double(1.0),
